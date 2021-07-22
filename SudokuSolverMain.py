f6b4c9accceb66a30ed4882296de9df7defa1d00
Zephyrum-Alsend <isfaryl@gmail.com>
2021-07-22 04:54:39 -0400

    File Validation
    It works, but it doesn't give as much diagnostic info to the user as I'd like.  Began separating it out into functions, so it can be called during the file reading phase (thereby having access to the file name during reporting).  ...I also think I should make an Error class at this point, but counter think that I'm overdesigning a simple Sudoku Solver.

--- SudokuSolverMain.py
+++ SudokuSolverMain.py
@@ -1,6 +1,6 @@
 #Author: Zephyrum Alsend
 #Sudoku Solver
-#Last Edited: 21/07/20
+#Last Edited: 21/07/22
 
 import copy
 from pathlib import Path
@@ -11,6 +11,9 @@ class Sudoku:
     def __init__(self, puzzle):
         self.__puzzle = puzzle
         self.__solved = copy.deepcopy(puzzle)
+        self.__size = 0
+        self.__subsize = 0
+        self.__mutable = 0
 
         #Ensure the board passed is 1.) usable data 2.) not breaking any rules
         try:
@@ -35,6 +38,12 @@ class Sudoku:
         except Exception as e:
             print("The board provided is invalid. Please double-check the data.")
             print(repr(e))
+            return
+
+        Illegal = self.isLegal()
+        if len(Illegal) > 0:
+            for e in Illegal:
+                print(e)
             return
 
         self.__mutable = self.getMutableCount()
@@ -75,50 +84,49 @@ class Sudoku:
     def getBoxCount(self):
         return self.__size
 
+    #Returns the total number of mutable squares in the puzzle.
     def getMutable(self):
         return self.__mutable
 
+    #Checks all immutable values obey the rules of Sudoku.
+    #Returns a list of messages dictating where illegal numbers are.
+    #Returns an empty list if everything is legal.
     def isLegal(self):
         Illegal = []
-        x = 0
-        y = 0
 
-        while y < self.__size:
-            Legal = True
-            z = self.__puzzle[y][x]
+        Columns = [[]]
+        for y in range(0, self.__size):
+            #Get a list of all !0 values in this row
+            temp = [n for n in self.__puzzle[y] if n != 0]
+            if len(set(temp)) != len(temp):
+                Illegal.append("Illegal placement in row " + str(y+1) + ".")
 
-            if z != 0:
-                #Get the row
-                for i in self.__puzzle[y]:
-                    if i == z and i != x:
-                        Legal = False
-                        break
+        for x in range(0, self.__size):
+            #Get a list of all !0 values in this column
+            temp = []
+            for y in self.__puzzle:
+                if y[x] != 0:
+                    temp.append(y[x])
+            if len(set(temp)) != len(temp):
+                Illegal.append("Illegal placement in column " + str(x+1) + ".")
 
-                #Get the column
-                for i in self.__puzzle:
-                    if i[x] == z and i != y:
-                        Legal = False
-                        break
+        #Get the boxes
+        for y in range(0, self.__subsize):
+            for x in range(0, self.__subsize):
+                temp = []
 
-                #Get the box
-                Bx = (x//self.__subsize) * self.__subsize
-                By = (y//self.__subsize) * self.__subsize
+                Bx = x * self.__subsize
+                By = y * self.__subsize
 
                 for i in range(By, By + self.__subsize):
                     for j in range(Bx, Bx + self.__subsize):
-                        if self.__puzzle[i][j] == z:
-                            Legal = False
-                            break
+                        if self.__puzzle[i][j] != 0:
+                            temp.append(self.__puzzle[i][j])
 
-                if not Legal:
-                    Illegal.append( tuple(x, y) )
+                if len(set(temp)) != len(temp):
+                    Illegal.append("Illegal placement in box " + str(y * self.__subsize + x + 1) + ".")
 
-            x += 1
-            if x >= self.__size:
-                x = 0
-                y += 1
-
-        return(Legal)
+        return(Illegal)
         
     #Solves the puzzle and stores the result in __solved.
     def solveSudoku(self):
@@ -260,6 +268,40 @@ def readSudokuPuzzles(strPath):
         f.close()
 
     return(Puzzles)
+
+def checkFile(puzzle):
+    #Ensure the board passed is 1.) usable data 2.) not breaking any rules
+    try:
+        size = len(puzzle)
+        subsize = size**0.5
+
+        if (subsize).is_integer():
+            subsize = int(subsize)
+        else:
+            raise Exception("Column length is not the square of a whole number.")
+            
+        for row in puzzle:
+            if len(row) != size:
+                raise Exception("Row length does not match column length.")
+                
+            for val in row:
+                if isinstance(val, int):
+                    if not ( 0 <= val <= size ):
+                        raise Exception("Integer value out of bounds.")
+                else:
+                    raise Exception("Non-integer data found.")
+
+    except Exception as e:
+        print("The board provided is invalid. Please double-check the data.")
+        print(repr(e))
+        return
+
+    Illegal = isLegal(puzzle)
+    if len(Illegal) > 0:
+        for e in Illegal:
+            print(e)
+        return
+
 
 ##########MAIN##########
 
